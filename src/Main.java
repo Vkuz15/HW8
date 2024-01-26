682c000e0a9f5dcc2bf3639328d1dfa44d60bd76
Владимир <vkuznecov1504@mail.ru>
2024-01-26 08:35:53 +0400

    Создали целочисленный массив с помощью ключевого слова new Создали массив с дробными числами Содали целочисленный массив

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Здача 1
+
+        int [] weights = new int [3];
+        weights [0] = 1;
+        weights [1] = 2;
+        weights [2] = 3;
+        System.out.println(weights[0]);
+        System.out.println(weights[1]);
+        System.out.println(weights[2]);
+
+        float [] i = {1.57f, 7.654f, 9.986f};
+        System.out.println(i[0]);
+        System.out.println(i[1]);
+        System.out.println(i[2]);
+
+        int [] birds = {1, 2, 3};
+        System.out.println(birds[0]);
+        System.out.println(birds[1]);
+        System.out.println(birds[2]);
     }
 }
