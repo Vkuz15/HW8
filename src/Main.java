1a8b7ef82cd841f9663ff6b2c6e3571ded806183
Владимир <vkuznecov1504@mail.ru>
2024-01-27 13:23:27 +0400

    Создали целочисленный массив с помощью ключевого слова new Создали массив с дробными числами Содали целочисленный массив Вывели в консоль результаты в строчку через запятую

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,40 @@ public class Main {
 
         //Здача 1
 
-        int [] weights = new int [3];
-        weights [0] = 1;
-        weights [1] = 2;
-        weights [2] = 3;
+        int[] weights = new int[3];
+        weights[0] = 1;
+        weights[1] = 2;
+        weights[2] = 3;
         System.out.println(weights[0]);
         System.out.println(weights[1]);
         System.out.println(weights[2]);
 
-        float [] i = {1.57f, 7.654f, 9.986f};
+        float[] i = {1.57f, 7.654f, 9.986f};
         System.out.println(i[0]);
         System.out.println(i[1]);
         System.out.println(i[2]);
 
-        int [] birds = {1, 2, 3};
+        int[] birds = {1, 2, 3};
         System.out.println(birds[0]);
         System.out.println(birds[1]);
         System.out.println(birds[2]);
+
+        //Задача 2
+
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        System.out.print(numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
+
+        System.out.println(" ");
+
+        float[] a = {1.57f, 7.654f, 9.986f};
+        System.out.print(a[0] + ", " + a[1] + ", " + a[2]);
+
+        System.out.println(" ");
+
+        int[] t = {1, 2, 3};
+        System.out.print(t[0] + ", " + t[1] + ", " + t[2]);
     }
 }
